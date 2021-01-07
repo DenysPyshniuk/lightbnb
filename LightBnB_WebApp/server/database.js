6828a422d4db01561b26f9e2b8e8ce019ef3b4b7
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-01-07 23:10:27 +0000

    Add options to search properties

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -112,11 +112,36 @@ const getAllProperties = function(options, limit = 10) {
     queryParams.push(`%${options.city}%`);
     queryString += `WHERE city LIKE $${queryParams.length} `;
   }
+  //3.1
+  if(options.owner_id) {
+    queryParams.push(options.owner_id)
+      if (queryParams.length === 1) {
+        queryString += `WHERE owner_id = $${queryParams.length}`;
+      } else {
+        queryString += `AND owner_id = $${queryParams.length}`;
+      }
+  }
+  //3.2
+  if(options.minimum_price_per_night && options.maximum_price_per_night){
+    queryParams.push(options.minimum_price_per_night *100, options.maximum_price_per_night *100);
+    if (queryParams.length === 2) {
+      queryString += `WHERE cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `;
+    } else {
+      queryString += `AND cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `;
+    }
+  }
+  queryString += 'GROUP BY properties.id '
+
+  //3.3
+  if(options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length}`;
+  }
 
   // 4
   queryParams.push(limit);
   queryString += `
-  GROUP BY properties.id
+
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
